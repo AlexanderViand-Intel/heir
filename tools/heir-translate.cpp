3cdc1f74b0f2e9aa2bd939f92d072e9e7bfbc1d8
Alexander Viand <alexander.viand@intel.com>
2024-07-01 15:16:12 -0700

    WIP: add PISAEmitter

--- tools/heir-translate.cpp
+++ tools/heir-translate.cpp
@@ -2,6 +2,7 @@
 #include "lib/Target/Metadata/MetadataEmitter.h"
 #include "lib/Target/OpenFhePke/OpenFhePkeEmitter.h"
 #include "lib/Target/OpenFhePke/OpenFhePkeHeaderEmitter.h"
+#include "lib/Target/PISA/PISAEmitter.h"
 #include "lib/Target/TfheRust/TfheRustEmitter.h"
 #include "lib/Target/TfheRustBool/TfheRustBoolEmitter.h"
 #include "lib/Target/Verilog/VerilogEmitter.h"
@@ -23,6 +24,10 @@ int main(int argc, char **argv) {
   // OpenFHE
   mlir::heir::openfhe::registerToOpenFhePkeTranslation();
   mlir::heir::openfhe::registerToOpenFhePkeHeaderTranslation();
+
+  // PISA
+  mlir::heir::pisa::registerToPISAInputsTranslation();
+  mlir::heir::pisa::registerToPISATranslation();
 
   return failed(mlir::mlirTranslateMain(argc, argv, "HEIR Translation Tool"));
 }
