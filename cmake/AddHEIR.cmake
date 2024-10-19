c109a0dfa996ef14c55a64364fd05c9bc41d81f3
Alexander Viand <alexander.viand@intel.com>
2024-10-19 17:10:29 +0000

    cleanup and fixes for automagical tablegen defs

--- cmake/AddHEIR.cmake
+++ cmake/AddHEIR.cmake
@@ -7,6 +7,33 @@ include(AddMLIR)
 include(AddLLVM)
 include(TableGen)
 
+# tablegen dialect helper "template"
+# For a dialect_name ABC and file_suffix Suffix it checks if ABCSuffix.td exists and if so,
+# generates the corresponding .h.inc and .cpp.inc files using -gen-cmd-declcs / -gen-cmd-defs
+# It adds ABC_CopyHeaders as a depenceny of the new tablegen target ABC_SuffixIncGen
+# and adds the new tablegen target as a dependency of ABC_AllIncGen
+macro(heir_dialect_tablegen dialect_name file_suffix cmd)
+  if(EXISTS "${original_current_source_dir}/${dialect_name}${file_suffix}.td")
+    set(LLVM_TARGET_DEFINITIONS ${dialect_name}${file_suffix}.td)
+    mlir_tablegen(${dialect_name}${file_suffix}.h.inc
+                  -gen-${cmd}-decls
+                  -dialect=${dialect_namespace}
+                  EXTRA_INCLUDES
+                  ${dialect_include_dirs}
+                )
+    mlir_tablegen(${dialect_name}${file_suffix}.cpp.inc
+                   -gen-${cmd}-defs
+                   -dialect=${dialect_namespace}
+                  EXTRA_INCLUDES
+                  ${dialect_include_dirs}
+                  )
+    add_public_tablegen_target(${dialect_target}_${file_suffix}IncGen)
+    add_dependencies(${dialect_target}_${file_suffix}IncGen ${dialect_target}_CopyHeaders)
+    add_dependencies(${dialect_target}_AllIncGen ${dialect_target}_${file_suffix}IncGen)
+  endif()
+endmacro()
+
+
 # custom helper for HEIR Dialects that follow our naming conventions
 # This effectively behaves like a combination of add_mlir_dialect (tablegen
 # and add_mlir_dialect_library (C++ library) from MLIR's AddMLIR.cmake module
@@ -20,109 +47,63 @@ include(TableGen)
 # unless something changes with the macros in AddMLIR.cmake / AddLLVM.cmake upstream
 function(add_AUTOMAGIC_heir_dialect dialect_name dialect_namespace)
 
-  # Name of the dialect target (will be created later)
+  # Name of the dialect target (actual target will be created later)
   set(dialect_target "heir__Dialect__${dialect_name}")
 
+  # Bazel-style copying of sources to a new target-specific binary directory
   # if we're not doing an in-source build
   # (i.e., the source and build directories are different),
   # Create all tablegen in a target-specific build directory
   set(original_current_binary_dir ${CMAKE_CURRENT_BINARY_DIR})
-  set(original_currrent_source_dir ${CMAKE_CURRENT_SOURCE_DIR})
-  if(NOT ${CMAKE_CURRENT_SOURCE_DIR} STREQUAL ${CMAKE_CURRENT_BINARY_DIR})
+  set(original_current_source_dir ${CMAKE_CURRENT_SOURCE_DIR})
+  if(NOT ${original_current_source_dir} STREQUAL ${original_current_binary_dir})
     # get the relative path from the source directory to the current directory
     file(RELATIVE_PATH cur_src_dir_rel ${CMAKE_SOURCE_DIR} ${CMAKE_CURRENT_SOURCE_DIR})
     # create a target-specific build directory at CMAKE_BINARY_DIR/TARGET/cur_src_dir_rel
-    set(custom_bianry_dir_prefix ${CMAKE_BINARY_DIR}/target_include/${dialect_target})
-    set(custom_binary_dir ${custom_bianry_dir_prefix}/${cur_src_dir_rel})
+    set(custom_binary_dir_prefix ${CMAKE_BINARY_DIR}/target_include/${dialect_target})
+    set(custom_binary_dir ${custom_binary_dir_prefix}/${cur_src_dir_rel})
     set(CMAKE_CURRENT_BINARY_DIR ${custom_binary_dir})
     set(CMAKE_CURRENT_SOURCE_DIR ${custom_binary_dir})
     message(STATUS "Creating custom binary directory for ${dialect_target} at ${custom_binary_dir}")
   endif()
 
+  # TODO: Grab the list of dependencies passed in and grab all their include directories
+  set(dialect_include_dirs  ${custom_binary_dir_prefix}) # FIXME
+  message(STATUS "Dialect include dirs for ${dialect_name}: ${dialect_include_dirs}")
+
   # Copy Headers/etc to ${custom_binary_dir}
   # Yes, globbing is bad, but we're doing this just for
   # Ninja's overly picky dependency graph tracking,
   # i.e., to define the "BYPRODUCTS" of the custom target
-  file(GLOB dialect_files LIST_DIRECTORIES false RELATIVE ${original_currrent_source_dir} ${original_currrent_source_dir}/*)
+  file(GLOB dialect_files LIST_DIRECTORIES false RELATIVE ${original_current_source_dir} ${original_current_source_dir}/*)
   list(TRANSFORM dialect_files PREPEND ${custom_binary_dir}/)
   add_custom_target(${dialect_target}_CopyHeaders
-    COMMAND ${CMAKE_COMMAND} -E copy_directory ${original_currrent_source_dir} ${custom_binary_dir}/
+    COMMAND ${CMAKE_COMMAND} -E copy_directory ${original_current_source_dir} ${custom_binary_dir}/
     BYPRODUCTS ${dialect_files}
-    COMMENT "Copying ${dialect} files from ${original_currrent_source_dir} to ${custom_binary_dir}"
+    COMMENT "Copying files from ${original_current_source_dir} to ${custom_binary_dir}"
   )
 
-  # TableGen target
+  # TableGen group target
   add_custom_target(${dialect_target}_AllIncGen)
-
   # Main Dialect definition
-  set(LLVM_TARGET_DEFINITIONS ${dialect_name}Dialect.td)
-  mlir_tablegen(${dialect_name}Dialect.h.inc -gen-dialect-decls -dialect=${dialect_namespace})
-  mlir_tablegen(${dialect_name}Dialect.cpp.inc -gen-dialect-defs -dialect=${dialect_namespace})
-  add_public_tablegen_target(${dialect_target}_DialectIncGen)
-  add_dependencies(${dialect_target}_DialectIncGen ${dialect_target}_CopyHeaders)
-  add_dependencies(${dialect_target}_AllIncGen ${dialect_target}_DialectIncGen)
-
+  heir_dialect_tablegen(${dialect_name} Dialect dialect)
   # Ops
-  if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${dialect_name}Ops.td")
-    set(LLVM_TARGET_DEFINITIONS ${dialect_name}Ops.td)
-    mlir_tablegen(${dialect_name}Ops.h.inc -gen-op-decls -dialect=${dialect_namespace})
-    mlir_tablegen(${dialect_name}Ops.cpp.inc -gen-op-defs -dialect=${dialect_namespace})
-    add_public_tablegen_target(${dialect_target}_OpsIncGen)
-    add_dependencies(${dialect_target}_OpsIncGen ${dialect_target}_CopyHeaders)
-    add_dependencies(${dialect_target}_AllIncGen ${dialect_target}_OpsIncGen)
-  endif()
-
+  heir_dialect_tablegen(${dialect_name} Ops op)
   # Types
-  if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${dialect_name}Types.td")
-    set(LLVM_TARGET_DEFINITIONS ${dialect_name}Types.td)
-    mlir_tablegen(${dialect_name}Types.h.inc -gen-typedef-decls -dialect=${dialect_namespace})
-    mlir_tablegen(${dialect_name}Types.cpp.inc -gen-typedef-defs -dialect=${dialect_namespace})
-    add_public_tablegen_target(${dialect_target}_TypesIncGen)
-    add_dependencies(${dialect_target}_TypesIncGen ${dialect_target}_CopyHeaders)
-    add_dependencies(${dialect_target}_AllIncGen ${dialect_target}_TypesIncGen)
-  endif()
-
-  # Attributes
-  if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${dialect_name}Attributes.td")
-    set(LLVM_TARGET_DEFINITIONS ${dialect_name}Attributes.td)
-    mlir_tablegen(${dialect_name}Attributes.h.inc -gen-attrdef-decls -attrdefs-dialect=${dialect_namespace})
-    mlir_tablegen(${dialect_name}Attributes.cpp.inc -gen-attrdef-defs -attrdefs-dialect=${dialect_namespace})
-    add_public_tablegen_target(${dialect_target}_AttributesIncGen)
-    add_dependencies(${dialect_target}_AttributesIncGen ${dialect_target}_CopyHeaders)
-    add_dependencies(${dialect_target}_AllIncGen ${dialect_target}_AttributesIncGen)
-  endif()
-
-  #Enums
-  if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${dialect_name}Enums.td")
-    set(LLVM_TARGET_DEFINITIONS ${dialect_name}Enums.td)
-    mlir_tablegen(${dialect_name}Enums.h.inc -gen-enum-decls)
-    mlir_tablegen(${dialect_name}Enums.cpp.inc -gen-enum-defs)
-    add_public_tablegen_target(${dialect_target}_EnumsIncGen)
-    add_dependencies(${dialect_target}_EnumsIncGen ${dialect_target}_CopyHeaders)
-    add_dependencies(${dialect_target}_AllIncGen ${dialect_target}_EnumsIncGen)
-  #Enums (but from Attributes file) #TODO (build): Deprecate this pattern
-  elseif(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${dialect_name}Attributes.td")
-    set(LLVM_TARGET_DEFINITIONS ${dialect_name}Attributes.td)
-    mlir_tablegen(${dialect_name}Enums.h.inc -gen-enum-decls)
-    mlir_tablegen(${dialect_name}Enums.cpp.inc -gen-enum-defs)
-    add_public_tablegen_target(${dialect_target}_EnumsIncGen)
-    add_dependencies(${dialect_target}_EnumsIncGen ${dialect_target}_CopyHeaders)
-    add_dependencies(${dialect_target}_AllIncGen ${dialect_target}_EnumsIncGen)
-  endif()
-
+  heir_dialect_tablegen(${dialect_name} Types typedef)
   # Type Interfaces
-  if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/${dialect_name}TypeInterfaces.td")
-    set(LLVM_TARGET_DEFINITIONS ${dialect_name}TypeInterfaces.td)
-    mlir_tablegen(${dialect_name}TypeInterfaces.h.inc -gen-type-interface-decls -name ${dialect_namespace})
-    mlir_tablegen(${dialect_name}TypeInterfaces.cpp.inc -gen-type-interface-defs -name ${dialect_namespace})
-    add_public_tablegen_target(${dialect_target}_TypeInterfacesIncGen)
-    add_dependencies(${dialect_target}_TypeInterfacesIncGen ${dialect_target}_CopyHeaders)
-    add_dependencies(${dialect_target}_AllIncGen ${dialect_target}_TypeInterfacesIncGen)
-  endif()
+  heir_dialect_tablegen(${dialect_name} TypeInterfaces type-interface)
+  # Attributes
+  heir_dialect_tablegen(${dialect_name} Attributes attrdef)
+  #Enums
+  heir_dialect_tablegen(${dialect_name} Enums enum)
+  #TODO (build): fix the dialect that puts Enums in the Attributes.td file!
+
+
 
   # Reset the directories to the original values
   set(CMAKE_CURRENT_BINARY_DIR ${original_current_binary_dir})
-  set(CMAKE_CURRENT_SOURCE_DIR ${original_currrent_source_dir})
+  set(CMAKE_CURRENT_SOURCE_DIR ${original_current_source_dir})
 
   #### C++ Library
 
